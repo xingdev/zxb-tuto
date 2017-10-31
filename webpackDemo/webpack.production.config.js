80d6d3dc7c69ff67dbc95f10d5830d62039d79f9
xingbo zhang <zxb@ekauibao.com>
2017-10-31 11:28:01 +0800

    清楚缓存

--- webpackDemo/webpack.production.config.js
+++ webpackDemo/webpack.production.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: __dirname + '/app/main.js', //已多次提及的唯一入口文件
   output: {
     path: __dirname + '/build',
-    filename: 'bundle.js'
+    filename: 'bundle-[hash].js'
   },
   devtool: 'eval-source-map',
   devServer: {
