89cebb49df8998c8ba788e3512754e4f2b209782
xingbo zhang <zxb@ekauibao.com>
2017-11-03 19:37:26 +0800

    test

--- node/l7/table.js
+++ node/l7/table.js
@@ -1,12 +1,33 @@
+var stacks = []
 function get (path, fn) {
-  if (path === '/1') {
-    fn()
-  }
+  stacks.push({
+    method: 'get',
+    path,
+    fn
+  })
 }
 
 function post (path, fn) {
-  if (path === '/3') {
-    fn()
-  }
+  stacks.push({
+    method: 'post',
+    path,
+    fn
+  })
 }
+
+function listen (url) {
+
+}
+
+get('/path1', function () {
+  console.log('get + /path1')
+})
+
+get('/path2', function () {
+  console.log('get + /path2')
+})
+
+post('/path2', function () {
+  console.log('post + /path2')
+})
 
